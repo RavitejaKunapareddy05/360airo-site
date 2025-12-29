269baca17eb82fa79e18e29ef5785e74c3ed8101
athulitsaoirse-cel <ravitejakunapareddy791@gmail.com>
2025-12-29 23:41:40 +0530

    Fix netlify.toml: remove invalid timeout config, set in next.config.js instead

--- next.config.js
+++ next.config.js
@@ -1,10 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
- 
   eslint: {
     ignoreDuringBuilds: true,
   },
   images: { unoptimized: true },
+  serverless: {
+    timeout: 60,
+  },
 };
 
 module.exports = nextConfig;
